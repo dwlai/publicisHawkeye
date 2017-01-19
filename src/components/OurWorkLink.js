d2aace010c19fea1e1505a5248a17cd8c86c2c2b
davidlai <david.lai@evermight.com>
2017-01-19 15:25:14 -0500

    added our-work links

---
+++ src/components/OurWorkLink.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default class OurWorkLink extends React.Component{
+
+	render(){
+		return(
+				<li><Link to="our-work">Our Work</Link></li>
+				
+				);
+	}
+
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Intro from '../components/Intro'
+import OurWorkLink from '../components/OurWorkLink'
 import Footer from '../components/Footer'
 import { Link } from 'react-router'
 
@@ -15,7 +16,19 @@ export default class Home extends React.Component{
 				<div id="about">
 					<div>WE ARE A 1:1 CUSTOMER ENGAGEMENT AGENCY LOCATED IN TORONTO</div>
 					<Intro />
-					<Link to="our-work">Our Work</Link>
+					<div>Our Work</div>
+					<div> 
+						<h2>See our work </h2>
+						<p> A good look at what we've done so far. (And a brief glimpse into what we can do.</p>
+					</div>
+					<ul>
+						<OurWorkLink />
+						<OurWorkLink />
+						<OurWorkLink />
+						<OurWorkLink />
+						<OurWorkLink />
+						<OurWorkLink />
+					</ul>
 				</div>
 				<Footer/>
 			</div>
