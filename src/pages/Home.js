eccc1f1ecd3e8d4abac79e7f1a954f00c5944701
davidlai <david.lai@evermight.com>
2017-01-20 12:16:39 -0500

    fixed about div end tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,8 +16,10 @@ export default class Home extends React.Component{
 				<Header />
 				<div id="about">
 					<div>WE ARE A 1:1 CUSTOMER ENGAGEMENT AGENCY LOCATED IN TORONTO</div>
+				</div>
 				<Intro />
-					<div id = "ourWork">Our Work</div>
+				<div id = "ourWork">
+					<p>Our Work</p>
 					<div> 
 						<h2>See our work </h2>
 						<p> A good look at what we've done so far. (And a brief glimpse into what we can do.</p>
@@ -30,17 +32,16 @@ export default class Home extends React.Component{
 						<OurWorkLink />
 						<OurWorkLink />
 					</ul>
-					<div id="team">
-						<h2>OUR LEADERSHIP TEAM</h2>
-						<p> Meet the men and women in charge of it all.</p>
-						
-						<ul>
-							<TeamLink />
-							<TeamLink />
-							<TeamLink />
-							<TeamLink />
-						</ul>
-					</div>
+				</div>
+				<div id="team">
+					<h2>OUR LEADERSHIP TEAM</h2>
+					<p> Meet the men and women in charge of it all.</p>			
+					<ul>
+						<TeamLink />
+						<TeamLink />
+						<TeamLink />
+						<TeamLink />
+					</ul>
 				</div>
 				<Footer/>
 			</div>
