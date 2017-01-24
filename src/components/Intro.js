90cd27de2ed37da07cccc7bdedf2b64391e95f88
davidlai <david.lai@evermight.com>
2017-01-24 14:15:10 -0500

    image change tested

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,13 +9,15 @@ export default class Intro extends React.Component{
 			super();
 			this.state = {
 						title: "WE CREATE RELATIONSHIP MOMENTS",
-						content: "From grand gestures to “just because”, every moment matters in defining a relationship – whether in our personal lives or in business. It’s these moments that we seize on. It’s these moments that make anyone feel special, heard, and compelled to take immediate action. And together, it’s these moments that ultimately determine how your relationship grows. "
+						content: "From grand gestures to “just because”, every moment matters in defining a relationship – whether in our personal lives or in business. It’s these moments that we seize on. It’s these moments that make anyone feel special, heard, and compelled to take immediate action. And together, it’s these moments that ultimately determine how your relationship grows. ",
+						image:"./assets/img/introImage.png"
 			};
 	}
 	
 	changeContent(){
 		this.setState({	title: "NEW CONTENT",
-						content :"Heres some new content"
+						content :"Heres some new content",
+						image: "./assets/img/hi.png"
 						});
 	}
 	
@@ -23,7 +25,7 @@ export default class Intro extends React.Component{
 		return (
 				<div id ="introSection" className="container-fluid">
 					<div className="row">
-						<IntroPic image="./assets/img/introImage.png" />
+						<IntroPic image={this.state.image} />
 						<div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 							<ul>
 								<IntroLink changeContent= {this.changeContent.bind(this)} title={["OUR",<br/>, "DIFFERENCE"]}/>
