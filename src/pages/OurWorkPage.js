a715be5822fceb7aef0e08b5d7a8e3a5a88a663e
davidlai <david.lai@evermight.com>
2017-01-26 14:18:20 -0500

    html markup for ourworks page

--- src/pages/OurWorkPage.js
+++ src/pages/OurWorkPage.js
@@ -5,10 +5,22 @@ export default class OurWorkPage extends React.Component{
 	
 	render(){
 		return(
-			<div>
-				<h1>OUR WORK</h1>
-				<Link to="/">Back</Link>
-			</div>
+				<div className="ourWorkPage">
+					<div className="container-fluid">
+						<div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+							<p>OUR WORK</p>
+							<Link className="back" to="/#ourWork">"BACK"</Link>
+							<h1>TITLE</h1>
+							<h2>SUB TITLE</h2>
+							<p> <span>Intelligence:</span> Content</p>
+							<p> <span>Relevance:</span> Content</p>
+							<p> <span>Action:</span> Content</p>
+						</div>
+						<div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+							<img alt="content" />
+						</div>
+					</div>
+				</div>
 			);
 	}
 }
