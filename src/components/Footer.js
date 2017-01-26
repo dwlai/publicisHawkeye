e1c56a825766422fa832e616380951dddcadf41c
davidlai <david.lai@evermight.com>
2017-01-26 13:11:47 -0500

    imported footer images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,11 @@ export default class Footer extends React.Component{
 			<footer id="footer">	
 				<p>GET IN TOUCH</p>
 					<ul>
-						<li><img alt="map"/> 111 Queen St E, Suite 200 Toronto, ON M5C 1S2</li>
-						<li><img alt="phone"/>(416) 925 -7733</li>
-						<li><img alt="email"/> hello@publicishawkeye.ca</li>
+						<li><img src="./assets/img/map.png" alt="map"/> 111 Queen St E, Suite 200 Toronto, ON M5C 1S2</li>
+						<li><img src="./assets/img/phone.png" alt="phone"/>(416) 925 -7733</li>
+						<li><img src="./assets/img/email.png" alt="email"/> hello@publicishawkeye.ca</li>
 					</ul>
+					<img id="footerlogo" src="./assets/img/footerlogo.png" alt="footer logo" />
 			</footer>
 		);
 		
