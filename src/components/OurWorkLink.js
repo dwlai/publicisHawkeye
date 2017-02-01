c4e58347deb74f8ae0e82f79c22bb63128f52975
Ryan Yao <ryanysyao@gmail.com>
2017-02-01 13:10:29 -0500

    our work section style updates

--- src/components/OurWorkLink.js
+++ src/components/OurWorkLink.js
@@ -12,6 +12,7 @@ export default class OurWorkLink extends React.Component{
 				<div className="work-individual col-sm-6">
 					<Link to={"our-work-page/" + this.props.title}> {/*<img src={this.props.image} alt={this.props.title}/> */}
             <div className="work-img" style={divStyle} title={this.props.title}>&nbsp;</div>
+            <div className="work-overlay"><h2>{this.props.title}</h2></div>
           </Link>
 				</div>
 				
