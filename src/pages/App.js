6a1e750e37b9cd9a849b30aafa6654f7b33cff98
davidlai <david.lai@evermight.com>
2017-02-06 17:08:47 -0500

    began adding react transition group

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 
 
 
 
 export default class App extends React.Component {
     render() {
+			
+			const {location } = this.props;
+			console.log(location.pathname);
+			
         return (
 			<div>
-				{this.props.children}
+			<ReactCSSTransitionGroup component='div' transitionName="fade" transitionEnterTimeout={3000} transitionLeaveTimeout={3000}>
+				{React.cloneElement(this.props.children, {key: location.pathname, parentComponent:this})}
+			</ReactCSSTransitionGroup>
 			</div>
 
         );
