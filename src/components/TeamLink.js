3bd3dc05240f43b0ea0e0dd0d6add47fde756dc3
Ryan Yao <ryanysyao@gmail.com>
2017-03-02 20:23:52 -0500

    team section update

--- src/components/TeamLink.js
+++ src/components/TeamLink.js
@@ -4,7 +4,7 @@ export default class TeamLink extends React.Component{
 	
 	render(){
 		return(
-				<div className="team-individual col-sm-2">
+				<div className={'team-individual col-sm-3 ' + this.props.className}>
 					<div className={this.props.className}>
 						<h4><em>{this.props.name}</em></h4>
 						<p>{this.props.title}</p>
